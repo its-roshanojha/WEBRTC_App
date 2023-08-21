47a881a9a066ed65f0f34342c3513a6664bba8b3
roshan <roshan@samarth.ac.in>
2023-08-21 11:57:55 +0530

    Done with the lobby design and added boostrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import Lobby from './screens/Lobby';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mt-3">
       <Routes>
-        <Route path="/" element={'HI'} />
+        <Route path="/" element={<Lobby/>} />
         {/* <Route path="/room/:roomId" element={<RoomPage />} /> */}
       </Routes>
     </div>
---
+++ client/src/screens/Lobby.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Lobby = () => {
+  const [email, setEmail] = useState("");
+  const [room, setRoom] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmitForm = useCallback(
+    (e) => {
+      e.preventDefault();
+      // socket.emit("room:join", { email, room });
+    }
+    //   [email, room, socket]
+  );
+
+  return (
+    <div className="pt-3">
+      <h1>Lobby</h1>
+      <form>
+        <div className="mb-3">
+          <label className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">
+          Room Number
+         </label>
+          <input
+            className="form-control"
+            id="exampleInputPassword1"
+            type="text"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          ENTER THE ROOM
+        </button>
+      </form>
+      
+    </div>
+  );
+};
+
+export default Lobby;
