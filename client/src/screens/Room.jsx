d7f8fdb39cb284ffe332c667e7577448206cadb4
roshan <roshan@samarth.ac.in>
2023-08-22 11:25:30 +0530

    send the stream and before that negotiation done is done again

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -46,33 +46,88 @@ const Room = () => {
         [socket]
       );
 
+     //   STEP 7 =>SEND STREAM AFTER CALL ACCEPTED. Peer.Peer will send the stream to different user
+    const sendStreams = useCallback(() => {
+        for (const track of myStream.getTracks()) {
+          peer.peer.addTrack(track, myStream);
+        }
+     }, [myStream]);
+
     //  STEP:6 => Call:Accepted 
     const handleCallAccepted = useCallback(
         ({ from, ans }) => {
           peer.setLocalDescription(ans);
           console.log("Call Accepted!");
-        //   sendStreams();
+          sendStreams();
         },
-        // [sendStreams]
+        [sendStreams]
       );
-    
 
+    //   STEP:9 => Before Negotiation we need to de-register the event listerner for sending streams and also for negotiation we are sending offer again
+     const handleNegoNeeded = useCallback(async () => {
+        const offer = await peer.getOffer();
+        socket.emit("peer:nego:needed", { offer, to: remoteSocketId });
+      }, [remoteSocketId, socket]);
+
+    //   STEP:9 (only)
+      useEffect(() => {
+        peer.peer.addEventListener("negotiationneeded", handleNegoNeeded);
+        return () => {
+          peer.peer.removeEventListener("negotiationneeded", handleNegoNeeded);
+        };
+      }, [handleNegoNeeded]);
+
+      const handleNegoNeedIncomming = useCallback(
+        async ({ from, offer }) => {
+          const ans = await peer.getAnswer(offer);
+          socket.emit("peer:nego:done", { to: from, ans });
+        },
+        [socket]
+      );
+
+      const handleNegoNeedFinal = useCallback(async ({ ans }) => {
+        await peer.setLocalDescription(ans);
+      }, []);
+
+    //   STEP:8 => Jab dusre user ke paas  track chala jayga to usse listener se handle karne ke liye\
+    // WE ARE SENDING THE STREAM BUT A NEGOTIATION NEEDED. Basically ye dono ko aapas me re-connect karna ha.
+      useEffect(() => {
+        peer.peer.addEventListener("track", async (e) => {
+          const remoteStream = e.streams;
+          console.log("GOT TRACKS!!");
+          setRemoteStream(remoteStream[0]);
+        });
+      }, []);
+
+    
+    // Everything is initialising from here 
         useEffect(() => {
             socket.on("user:joined", handleUserJoined);
             socket.on("incomming:call", handleIncommingCall);
             socket.on("call:accepted", handleCallAccepted);
+    // STEP:10 => Handling the peer connection offer and sending an answe 
+            socket.on("peer:nego:needed", handleNegoNeedIncomming);
+    // STEP: 11 => Setting the answer in local description
+            socket.on("peer:nego:final", handleNegoNeedFinal);
+
+    
 
             return () => {
             socket.off("user:joined", handleUserJoined);
             socket.off("incomming:call", handleIncommingCall);
-            socket.off("call:accepted", handleCallAccepted);    
+            socket.off("call:accepted", handleCallAccepted);
+            socket.off("peer:nego:needed", handleNegoNeedIncomming);
+            socket.off("peer:nego:final", handleNegoNeedFinal);
+
 
             };
         }, [
             socket,
             handleUserJoined,
             handleIncommingCall,
-
+            handleCallAccepted,
+            handleNegoNeedIncomming,
+            handleNegoNeedFinal
         ]);
 
 
@@ -81,6 +136,7 @@ const Room = () => {
          <div>
       <h1>Room Page</h1>
       <h4>{remoteSocketId ? "Connected" : "No one in room"}</h4>
+      {/* STEP:12 => Now our connection is done but aur send stream is not working so we will send the stream again with the help of button */}
       {myStream && <button onClick={sendStreams}>Send Stream</button>}
       {remoteSocketId && <button onClick={handleCallUser}>CALL</button>}
       {myStream && (
@@ -89,8 +145,8 @@ const Room = () => {
           <ReactPlayer
             playing
             muted
-            height="100px"
-            width="200px"
+            height="200px"
+            width="400px"
             url={myStream}
           />
         </>
@@ -101,8 +157,8 @@ const Room = () => {
           <ReactPlayer
             playing
             muted
-            height="100px"
-            width="200px"
+            height="200px"
+            width="400px"
             url={remoteStream}
           />
         </>
